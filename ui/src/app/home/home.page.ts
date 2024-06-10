3cf2d4f3f33bcf3a459cffb195ab40d7111782d9
galwort <wthomasgorbett@gmail.com>
2024-06-10 18:22:45 -0400

    Refactor to more explicit variable setting

--- ui/src/app/home/home.page.ts
+++ ui/src/app/home/home.page.ts
@@ -6,25 +6,34 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  showSupportingDetails = false;
+  showTextInput = false;
+
+  buttonLabel = 'Start';
   imageFilename = 'bricks';
-  supportingDetails = false;
-  dialogue = "What's your company's name?";
   inputText = '';
-  companyName = '';
+  companyName = 'your company';
+  companyDescription = '';
+
   questionIndex = 0;
+  dialogue =
+    'Ground Floor is a Sims like game for building your own company that uses LLMs to generate elements of the game.';
 
   constructor() {}
 
-  onSubmit() {
-    if (this.questionIndex === 0) {
-      this.companyName = this.inputText;
-      this.dialogue = `What does ${this.companyName} do?`;
-      this.inputText = '';
-      this.questionIndex++;
-    } else {
-      console.log(
-        `Company Name: ${this.companyName}, Company Description: ${this.inputText}`
-      );
+  onButtonClick() {
+    switch (this.questionIndex) {
+      case 0:
+        this.dialogue = "What's your company's name?";
+        this.showTextInput = true;
+        this.buttonLabel = 'Next';
+        break;
+      case 1:
+        this.companyName = this.inputText;
+        this.dialogue = `What does ${this.companyName} do?`;
+        this.inputText = '';
+        break;
     }
+    this.questionIndex++;
   }
 }
