f468ebf4e9652b5b73b3f283f7ad565aca4a4585
galwort <wthomasgorbett@gmail.com>
2024-06-10 19:27:33 -0400

    Connect front end to back and format for job titles

--- ui/src/app/home/home.module.ts
+++ ui/src/app/home/home.module.ts
@@ -3,17 +3,16 @@ import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 import { FormsModule } from '@angular/forms';
 import { HomePage } from './home.page';
+import {
+  provideHttpClient,
+  withInterceptorsFromDi,
+} from '@angular/common/http';
 
 import { HomePageRoutingModule } from './home-routing.module';
 
-
 @NgModule({
-  imports: [
-    CommonModule,
-    FormsModule,
-    IonicModule,
-    HomePageRoutingModule
-  ],
-  declarations: [HomePage]
+  imports: [CommonModule, FormsModule, IonicModule, HomePageRoutingModule],
+  declarations: [HomePage],
+  providers: [provideHttpClient(withInterceptorsFromDi())],
 })
 export class HomePageModule {}
--- ui/src/app/home/home.page.ts
+++ ui/src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
@@ -8,18 +9,20 @@ import { Component } from '@angular/core';
 export class HomePage {
   showSupportingDetails = false;
   showTextInput = false;
+  showButton = true;
 
   buttonLabel = 'Start';
   imageFilename = 'bricks';
   inputText = '';
   companyName = 'your company';
   companyDescription = '';
+  jobTitles: string[] = [];
 
   questionIndex = 0;
   dialogue =
     'Ground Floor is a Sims like game for building your own company that uses LLMs to generate elements of the game.';
 
-  constructor() {}
+  constructor(private http: HttpClient) {}
 
   onButtonClick() {
     switch (this.questionIndex) {
@@ -33,7 +36,42 @@ export class HomePage {
         this.dialogue = `What does ${this.companyName} do?`;
         this.inputText = '';
         break;
+      case 2:
+        this.companyDescription = this.inputText;
+        this.generateJobTitles(this.companyDescription);
+        this.showButton = false;
+        this.dialogue = `Generating job titles for ${this.companyName}...`;
+        this.inputText = '';
+        this.showTextInput = false;
+        break;
+      case 3:
+        this.dialogue =
+          'Ground Floor is a Sims like game for building your own company that uses LLMs to generate elements of the game.';
+        this.jobTitles = [];
+        this.questionIndex = -1;
+        this.showTextInput = false;
+        this.buttonLabel = 'Start';
+        break;
     }
     this.questionIndex++;
+  }
+
+  generateJobTitles(description: string) {
+    const url = 'https://fa-groundfloor.azurewebsites.net/api/jobs';
+    const body = { company_description: description };
+
+    this.http.post<{ jobs: string[] }>(url, body).subscribe(
+      (response) => {
+        this.jobTitles = response.jobs;
+        this.dialogue = `Which positions would you like to hire for?`;
+        this.showButton = true;
+        this.buttonLabel = 'Interview';
+      },
+      (error) => {
+        console.error('Error generating job titles:', error);
+        this.dialogue =
+          'There was an error generating job titles. Please try again.';
+      }
+    );
   }
 }
